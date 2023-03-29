9bee5cdfddc2dd5eecb6497bc235d976103e2c66
Bima Bagaskhoro <bimabagas644@gmail.com>
2023-03-29 11:38:15 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FirstKmm"
+include(":androidApp")
+include(":shared")
